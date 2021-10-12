8a64b30f56aa467aa7a27c3ff68a39a59734ac8b
Adamtoo <adamtoo@hotmail.co.uk>
2021-10-12 22:21:23 +0100

    Finished first part of tutorial
    First part consists of: moving background, player jump movement, and obstacles placed randomly

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerBody;
+    public bool isOnGround = true;
+    [SerializeField]
+    public float jumpForce = 10;
+    [SerializeField]
+    private float gravityModifier;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
