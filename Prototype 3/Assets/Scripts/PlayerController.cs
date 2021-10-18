e16c5bcbae8351ebea40e54eb44bd1633f324ad2
Adamtoo <adamtoo@hotmail.co.uk>
2021-10-18 01:51:30 +0100

    Added animations to player
    Player now has running, jump and death animation

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -11,21 +11,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float gravityModifier;
     public bool gameOver;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
         
     }
@@ -40,6 +43,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game over!");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
