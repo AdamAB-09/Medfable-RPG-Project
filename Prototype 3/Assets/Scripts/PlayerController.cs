b1b614664cbbc2f7b845b92bea028daff25ab0c2
Adamtoo <adamtoo@hotmail.co.uk>
2021-10-18 01:18:13 +0100

    Updated scripts
    Objects now get destroyed offscreen and background is repeated constantly as player moves. Finally, player freezes when hitting an obstacle

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     [SerializeField]
     private float gravityModifier;
+    public bool gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +32,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game over!");
+            gameOver = true;
+        }
     }
 }
