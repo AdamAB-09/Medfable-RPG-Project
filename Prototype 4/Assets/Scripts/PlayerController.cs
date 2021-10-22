d66dade16362d7b281702759865b7bfff50133fd
Adamtoo <adamtoo@hotmail.co.uk>
2021-10-22 15:14:10 +0100

    Powerup for player
    Allow player to pick up powerup for a set duration of time - this powerup gives the player boosted strength

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerBody;
-    private float speed = 5;
+    [SerializeField]
+    private float speed;
     [SerializeField]
     private GameObject focalPoint;
+    [SerializeField]
+    private GameObject powerupIndicator;
+    private bool hasPowerup;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        powerupIndicator.transform.position = transform.position;
     }
 
     private void FixedUpdate()
@@ -25,4 +30,37 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         playerBody.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCountdown());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        { 
+            float powerupStrength = 10;
+            Rigidbody enemyBody = collision.gameObject.GetComponent<Rigidbody>();
+            //This gets the opposite vector that points away from the player
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position).normalized;
+            enemyBody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            
+        }
+    }
+
+    IEnumerator PowerupCountdown()
+    {
+        float countdownTimer = 6;
+        yield return new WaitForSeconds(countdownTimer);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
 }
