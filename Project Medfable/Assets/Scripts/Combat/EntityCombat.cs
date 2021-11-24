c1fb4b0c41ee4915662e3c314a198d243b793fa6
Adamtoo <adamtoo@hotmail.co.uk>
2021-11-24 19:24:41 +0000

    Created combat scripts
    Created multiple combat scripts and placeholder enemy to test when the player clicks on the enemy it returns a statement

---
+++ Project Medfable/Assets/Scripts/Combat/EntityCombat.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Medfable.Combat
+{
+    public class EntityCombat : MonoBehaviour
+    {
+        
+        // User will start attacking the target
+        public void Attack(CombatTarget target)
+        {
+            Debug.Log("Attacking");
+        }
+    }
+}
--- Project Medfable/Assets/Scripts/Controller/PlayerController.cs
+++ Project Medfable/Assets/Scripts/Controller/PlayerController.cs
@@ -1,3 +1,4 @@
+using Medfable.Combat;
 using Medfable.Movement;
 using UnityEngine;
 using UnityEngine.AI;
@@ -11,24 +12,49 @@ namespace Medfable.Controller
         void Update()
         {
             PlayerAnimation();
-            //Checks whether the left button which initaties player movement
+            PlayerCombat();
+            PlayerMovement();
+        }
+
+        // Allows the player to engage in combat with targets in the environment 
+        private void PlayerCombat()
+        {
+            //Checks whether left mouse button is pressed and gets all the raycast hits
             if (Input.GetMouseButtonDown(0))
             {
-                PlayerMovement();
+                Ray cameraRay = GetCameraRay();
+                RaycastHit[] hits = Physics.RaycastAll(cameraRay);
+
+                /* If one of the raycast hits has a CombatTarget script attached then allow
+                   player to attack
+                */
+                foreach (RaycastHit hit in hits)
+                {
+                    CombatTarget target = hit.collider.GetComponent<CombatTarget>();
+                    if (target != null)
+                    {
+                        GetComponent<EntityCombat>().Attack(target);
+                        break;
+                    }
+                }
             }
         }
 
         // Uses raycasting to move the player where the user clicks on the environment 
         private void PlayerMovement()
         {
-            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit target;
-            bool hasHit = Physics.Raycast(cameraRay, out target);
-
-            //Moves the player to where the raycast has collided by calling EntityMovement script
-            if (hasHit)
+            //Checks whether the left mouse button is pressed which initiates player movement
+            if (Input.GetMouseButtonDown(0))
             {
-                GetComponent<EntityMovement>().MoveTowards(target.point);
+                Ray cameraRay = GetCameraRay();
+                RaycastHit target;
+                bool hasHit = Physics.Raycast(cameraRay, out target);
+
+                //Moves the player to where the raycast has collided by calling EntityMovement script
+                if (hasHit)
+                {
+                    GetComponent<EntityMovement>().MoveTowards(target.point);
+                }
             }
         }
 
@@ -39,6 +65,12 @@ namespace Medfable.Controller
             Vector3 relativeVelocity = transform.InverseTransformDirection(GetComponent<NavMeshAgent>().velocity);
             float forwardSpeed = Mathf.Abs(relativeVelocity.z);
             GetComponent<Animator>().SetFloat("forwardSpeed", forwardSpeed);
+        }
+
+        // Returns the point of the ray where the player clicks on the screen
+        private Ray GetCameraRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
