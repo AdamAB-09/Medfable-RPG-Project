bd696c27692a4976b8ecc3d49bde13a5e8d5a010
Adamtoo <adamtoo@hotmail.co.uk>
2021-12-03 04:19:53 +0000

    Created a build for prototype 3

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -8,20 +8,23 @@ public class MoveLeft : MonoBehaviour
     private float speed = 30;
     private PlayerController playerControllerScript;
     private float leftBound = -15;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
+        // Initialise the Player Controller by locating the player in the scene
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Obstacles and background only moves left when player is alive
         if (!playerControllerScript.gameOver)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
+        // To reduce overhead, destroy obstacles that move past the player and are present off-screen
         if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
--- Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -6,9 +6,11 @@ public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
     private float repeatWidth;
+    
     // Start is called before the first frame update
     void Start()
     {
+        // Gets the starting position of the background and half its width
         startPos = transform.position;
         repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
@@ -16,6 +18,7 @@ public class RepeatBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move the background back to original position when it moves past half its width
         if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
