999f37b9d0ca17c59efccadeb6ebd4bd2c32fe93
Adamtoo <adamtoo@hotmail.co.uk>
2022-02-27 19:32:49 +0000

    Able to save/load between scenes
    The user can now load their most recent file from any scene they're in along with saving the most recent file

--- Project Medfable/Assets/Scripts/Saving/EntitySavable.cs
+++ Project Medfable/Assets/Scripts/Saving/EntitySavable.cs
@@ -1,26 +1,61 @@
+using Medfable.Core;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Medfable.Saving
 {
+    [ExecuteAlways]
     public class EntitySavable : MonoBehaviour
     {
+        [SerializeField]
+        private string guid = "";
+
+#if UNITY_EDITOR
+        /*Update will only be executed when in Unity Editor - when the game is 
+        * built, this code will not be executed. The GUID class variable is turned
+        * into a serialized object in order to be saved into a file
+        */
+        private void Update()
+        {
+            /*No changes will be made when the game is running or in prefab window - changes
+            * to GUID will only be done in editor mode
+            */
+            if (string.IsNullOrEmpty(gameObject.scene.path)) { return; }
+            if (Application.IsPlaying(gameObject)) { return; }
+
+            SerializedObject serializedObj = new SerializedObject(this);
+            SerializedProperty property = serializedObj.FindProperty("guid");
+            
+            //If the GUID is empty then it will generate a GUID for the object in editor mode
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObj.ApplyModifiedProperties();
+            }
+        }
+#endif
+
         //Gets the GUID of the savable entity
         public string GetGUID()
         {
-            return "";
+            return guid;
         }
 
         //Allows the current object state to be saved
         public object CatchObjState()
         {
-            print("Capturing state for: " + GetGUID());
-            return null;
+            return new SerializePosition(transform.position);
         }
 
         //Restores the prior state of the object
-        public void RestoreGameObj(object obj)
+        public void RestoreObjState(object obj)
         {
-            print("Restoring state for: " + GetGUID());
+            SerializePosition pos = (SerializePosition)obj;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = pos.GetVector3();
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<InteractionScheduler>().CancelCurrentAction();
         }
 
     }
--- Project Medfable/Assets/Scripts/Saving/SaveSystem.cs
+++ Project Medfable/Assets/Scripts/Saving/SaveSystem.cs
@@ -9,51 +9,78 @@ namespace Medfable.Saving
     public class SaveSystem : MonoBehaviour
     {
 
-        //Allows the player to save their current progress onto a file
+        //Merges previous game state with the current one and then saves the file
         public void Save(string saveFile)
         {
-            string path = Path.Combine(Application.persistentDataPath, saveFile);
-            print("Currently saving to " + path);
+            Dictionary<string, object> gameState = LoadFile(saveFile);
+            CatchGameState(gameState);
+            SaveFile(saveFile, gameState);
+        }
+
+        //Saves current file via serialization of the game's state
+        private void SaveFile(string saveFile, Dictionary<string, object> gameState)
+        {
+            string path = GetFilePath(saveFile);
             using (FileStream fileStream = File.Open(path, FileMode.Create))
             {
                 BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(fileStream, CatchGameState());
+                formatter.Serialize(fileStream, gameState);
             }
         }
 
-        //Allows the player to load up their most current file
+        //Allows the player to load up their most current file for the scene
         public void Load(string loadFile)
         {
-            string path = Path.Combine(Application.persistentDataPath, loadFile);
-            print("Loading data from " + path);
+            GetGameState(LoadFile(loadFile));
+        }
+
+        /*Loads up most current save file by deserializing the file otherwise if a
+        * load file doesn't exist it will create a new dict for whenever the user saves
+        */
+        private Dictionary<string, object> LoadFile(string loadFile)
+        {
+            string path = GetFilePath(loadFile);
+            if (!File.Exists(path))
+            {
+                return new Dictionary<string, object>();
+            }
+            
             using (FileStream fileStream = File.Open(path, FileMode.Open))
             {
                 BinaryFormatter formatter = new BinaryFormatter();
-                GetGameState(formatter.Deserialize(fileStream));
+                return (Dictionary<string, object>)formatter.Deserialize(fileStream);
             }
         }
 
-        //Restores a prior game state by checking all the savable entities in a saved dictionary
-        private void GetGameState(object gameState)
+        //Gets the file path of the save file
+        private string GetFilePath(string loadFile)
         {
-            Dictionary<string, object> dictState = (Dictionary<string, object>) gameState;
+            return Path.Combine(Application.persistentDataPath, loadFile);
+        }
+
+        //Restores a prior game state by checking all the savable entities in a saved dictionary
+        private void GetGameState(Dictionary<string, object> gameState)
+        {
             foreach (EntitySavable entity in FindObjectsOfType<EntitySavable>())
             {
-                entity.RestoreGameObj(dictState[entity.GetGUID()]);
+                string entityGuid = entity.GetGUID();
+                if (gameState.ContainsKey(entityGuid))
+                {
+                   entity.RestoreObjState(gameState[entityGuid]);
+                }
             }
         }
 
         /*Capture the current game state by getting all the savable entites and adding it to
         * a dictionary
         */
-        private object CatchGameState()
+        private void CatchGameState(Dictionary<string, object> gameState)
         {
-            Dictionary<string, object> dictState = new Dictionary<string, object>();
             foreach (EntitySavable entity in FindObjectsOfType<EntitySavable>())
             {
-                dictState[entity.GetGUID()] = entity.CatchObjState();
+                string entityGuid = entity.GetGUID();
+                gameState[entityGuid] = entity.CatchObjState();
             }
-            return dictState;
         }
     }
 }
